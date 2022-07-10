7e805467e4436c0a145ac672e4d57f71a97a69bf
KimeraMoses <kimeramoses001@gmail.com>
2022-07-10 20:09:59 +0300

    New: tested server with postman

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Hello from the server',
+  });
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(201).send('Posted successfuly');
+});
 
 const port = 3000;
 app.listen(port, () => {
