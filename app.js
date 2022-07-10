a034544554ff8bbf58fb0a235f49eb0368b5c2a3
KimeraMoses <kimeramoses001@gmail.com>
2022-07-10 22:04:53 +0300

    New: added morgan middleware to the project

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-//****MIDDLEWARE ****/
+//****MIDDLEWARES ****/
+app.use(morgan('dev'));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!!');
-  next();
-});
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
