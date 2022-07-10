1f6f4f34bf70c442da419b15ad73e0e40c3a5ecd
KimeraMoses <kimeramoses001@gmail.com>
2022-07-10 21:36:44 +0300

    New: implemented CRUD endpoints for the tours resource

--- app.js
+++ app.js
@@ -1,21 +1,111 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    message: 'Hello from the server',
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.status(201).send('Posted successfuly');
+app.get('/api/v1/tours/:id', (req, res) => {
+  const Id = req.params.id * 1;
+  if (Id > tours.length)
+    return res.status(404).json({ status: 'Fail', message: 'INVALID ID' });
+  //   const tour = tours.filter((el) => el.id === req.params.id * 1);
+  const tour = tours[Id];
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const Id = req.params.id * 1;
+  if (Id > tours.length || !tours[Id])
+    return res.status(404).json({ status: 'Fail', message: 'INVALID ID' });
+
+  tours[Id] = { ...tours[Id], ...req.body };
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err)
+        res.status(500).json({
+          status: 'fail',
+          message: 'Failed to save tour',
+        });
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: tours[Id],
+        },
+      });
+    }
+  );
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const Id = req.params.id * 1;
+  if (Id > tours.length || !tours[Id])
+    return res.status(404).json({ status: 'Fail', message: 'INVALID ID' });
+
+  tours.splice(Id, 1);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err)
+        res.status(500).json({
+          status: 'fail',
+          message: 'Failed to save tour',
+        });
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    }
+  );
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err)
+        res.status(500).json({
+          status: 'fail',
+          message: 'Failed to save tour',
+        });
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App runing on port ${port}...`);
+  console.log(`Server runing on port ${port}...`);
 });
